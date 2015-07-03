a5a0137703faf7640d240ef33623f33d8284eac7
jordan <ajmcmorris@gmail.com>
2015-07-03 10:49:11 -0400

    easel updates

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 //Javscript File
+window.onload = function(){
+
+	function init(){
+		//creating new stage instance
+		var stage = new createjs.Stage("myStage");
+
+		//making a circle
+		var circle = new createjs.Shape();
+
+		circle.graphics.beginFill("red").drawCircle(0,0,50);
+		circle.x = 100;
+		circle.y = 100;
+
+		stage.addChild(circle);
+		stage.update();
+	}
+	console.log('tings is runnin!');
+	init();
+};
